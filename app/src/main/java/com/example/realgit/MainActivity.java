fee9d1fe94a6fe6a76d0297a6223ed6c12278f42
UR <forumur1964@gmai.com>
2019-08-22 21:31:25 +0900

    Branch1

--- app/src/main/java/com/example/realgit/MainActivity.java
+++ app/src/main/java/com/example/realgit/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.realgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    View    viewBr1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //******klj
+        viewBr1=(View)findViewById(R.id.viewBr1);
+
     }
 }
